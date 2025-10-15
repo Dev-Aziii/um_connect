b10fdb28ca3ddf4592a9f3bf3f2622a69b6c9f09
dev_azi <azi26.dev@gmail.com>
2025-10-15 14:56:28 +0800

    Refactor Firebase options configuration; replace dotenv usage with hardcoded values for web, android, ios, macos, and windows

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load();
+  //await dotenv.load();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const ProviderScope(child: MyApp()));
